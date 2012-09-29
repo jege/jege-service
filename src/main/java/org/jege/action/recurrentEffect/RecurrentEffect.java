f5718133585ac8757269826bafaafab3a1518f4c
Paul Dijou <paul.dijou.dev@gmail.com>
2012-09-29 23:41:39 +0200

    Commit from JBoss Tools

--- src/main/java/org/jege/action/recurrentEffect/RecurrentEffect.java
+++ src/main/java/org/jege/action/recurrentEffect/RecurrentEffect.java
@@ -1,12 +1,8 @@
 package org.jege.action.recurrentEffect;
 
 import javax.persistence.Entity;
-import javax.time.Period;
 
-import org.hibernate.annotations.Type;
 import org.jege.abs.entity.AbstractEntity;
-import org.jege.action.effect.Effect;
-import org.jege.util.time.TimeUserType;
 
 @Entity
 public class RecurrentEffect extends AbstractEntity {
